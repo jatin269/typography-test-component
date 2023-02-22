739b4bb1bab861bf2e8dd696ee14796ca24b8d13
Jatin Jangra <jatin.jangra@farmart.co>
2023-02-22 15:58:57 +0530

    typography done

--- text.js
+++ text.js
@@ -1,105 +1,164 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 
+let defaultSize = "400";
+let fontWeights = {
+  fontSansRegular: defaultSize,
+  fontSansMedium: +defaultSize + 100 + "",
+  fontSansSemibold: +defaultSize + 200 + "",
+  fontSansBold: +defaultSize + 300 + "",
+};
+let fontSize = {
+  fs10: 10,
+  fs12: 12,
+  fs14: 14,
+  fs16: 16,
+  fs18: 18,
+  fs20: 20,
+  fs24: 24,
+  fs28: 28,
+  fs32: 32,
+};
+
+let letterSpacing = {
+  ls16: 16,
+  ls20: 20,
+  ls24: 24,
+  ls28: 28,
+  ls32: 32,
+  ls36: 36,
+  ls40: 40,
+};
+
+let defaultFontFamily = "NotoSans-Regular";
+
 export default function index({
-  type = "m",
+  type = "body",
+  typeVariant = "m12",
   text,
   textStyle,
-  langFontFamily = "NotoSans-Regular",
+  langFontFamily = defaultFontFamily,
   ...restProps
 }) {
   return (
     <Text
       allowFontScaling={false}
       {...restProps}
-      style={[styles[type], textStyle, { fontFamily: langFontFamily }]}
+      style={[
+        styles[type][typeVariant],
+        textStyle,
+        { fontFamily: langFontFamily },
+      ]}
     >
       {text}
     </Text>
   );
 }
 const styles = StyleSheet.create({
-  heading1: {
-    fontSize: 32,
-    lineHeight: 40,
+  body: {
+    m12: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansMedium,
+      lineHeight: letterSpacing.ls16,
+      fontSize: fontSize.fs12,
+    },
+    r14: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansRegular,
+      lineHeight: letterSpacing.ls20,
+      fontSize: fontSize.fs14,
+    },
+    r16: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansRegular,
+      lineHeight: letterSpacing.ls24,
+      fontSize: fontSize.fs16,
+    },
   },
+  Button: {
+    link: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansMedium,
 
-  heading2: {
-    fontSize: 16,
-    lineHeight: 24,
+      lineHeight: letterSpacing.ls24,
+      fontSize: fontSize.fs14,
+      textDecorationLine: "underline",
+    },
+    small: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansMedium,
+      lineHeight: letterSpacing.ls16,
+      fontSize: fontSize.fs12,
+    },
+    large: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansMedium,
+      lineHeight: letterSpacing.ls20,
+      fontSize: fontSize.fs14,
+    },
   },
-  heading3: {
-    fontSize: 28,
-    lineHeight: 36,
+  Subheading: {
+    sb14: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansMedium,
+      lineHeight: letterSpacing.ls20,
+      fontSize: fontSize.fs14,
+    },
+    m14: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansMedium,
+      lineHeight: letterSpacing.ls20,
+      fontSize: fontSize.fs14,
+    },
+    r16: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansRegular,
+      lineHeight: letterSpacing.ls24,
+      fontSize: fontSize.fs16,
+    },
+    sb12: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansSemibold,
+      lineHeight: letterSpacing.ls16,
+      fontSize: fontSize.fs12,
+    },
   },
-  heading4: {
-    fontSize: 24,
-    lineHeight: 32,
-  },
-  heading5: {
-    fontSize: 20,
-    lineHeight: 28,
-    fontWeight: "600",
-  },
-  heading6: {
-    fontSize: 18,
-    lineHeight: 26,
-    fontWeight: "600",
-  },
-  subTitle1: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: "400",
-  },
-  subTitle2: {
-    fontSize: 14,
-    lineHeight: 20,
-    fontWeight: "500",
-  },
-  subTitle3: {
-    fontSize: 14,
-    lineHeight: 20,
-    fontWeight: "600",
-  },
-  subTitle4: {
-    fontSize: 12,
-    lineHeight: 16,
-    fontWeight: "600",
-  },
-  button1: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: "500",
-  },
-  button2: {
-    fontSize: 14,
-    lineHeight: 24,
-    fontWeight: "500",
-  },
-  linkButton: {
-    fontSize: 14,
-    lineHeight: 24,
-    fontWeight: "500",
-    textDecorationLine: "underline",
-  },
-  body1: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: "400",
-  },
-  body2: {
-    fontSize: 14,
-    lineHeight: 20,
-    fontWeight: "400",
-  },
-  body3: {
-    fontSize: 12,
-    lineHeight: 16,
-    fontWeight: "500",
-  },
-  body4: {
-    fontSize: 10,
-    lineHeight: 16,
-    fontWeight: "600",
+  heading: {
+    sb16: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansSemibold,
+      lineHeight: letterSpacing.ls24,
+      fontSize: fontSize.fs16,
+    },
+    sb18: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansSemibold,
+      lineHeight: letterSpacing.ls24,
+      fontSize: fontSize.fs18,
+    },
+    m20: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansMedium,
+      lineHeight: letterSpacing.ls28,
+      fontSize: fontSize.fs20,
+    },
+    m24: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansMedium,
+      lineHeight: letterSpacing.ls32,
+      fontSize: fontSize.fs24,
+    },
+    r28: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansRegular,
+      lineHeight: letterSpacing.ls36,
+      fontSize: fontSize.fs28,
+    },
+    r32: {
+      fontFamily: fontSans,
+      fontWeight: fontWeights.fontSansRegular,
+      lineHeight: letterSpacing.ls40,
+      fontSize: fontSize.fs32,
+    },
   },
 });
